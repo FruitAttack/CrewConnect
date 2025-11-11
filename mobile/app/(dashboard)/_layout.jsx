4b86063b3a5e70770a7cd44f95b69b315e1e5d9e
Spencer Perry <u1431079@umail.utah.edu>
2025-11-11 15:06:12 -0700

    added Icons to nav bar

--- mobile/app/(dashboard)/_layout.jsx
+++ mobile/app/(dashboard)/_layout.jsx
@@ -1,8 +1,10 @@
 import { Tabs } from "expo-router"
 import { useColorScheme } from "react-native"
 import { Colors } from "../../constants/Colors"
-
-
+import Octicons from '@expo/vector-icons/Octicons';
+import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import Ionicons from '@expo/vector-icons/Ionicons';
 const DashboardLayout = () => {
   const colorScheme = useColorScheme()
   const theme = Colors[colorScheme]
@@ -23,16 +25,50 @@ const DashboardLayout = () => {
     >
       <Tabs.Screen 
       name="apps_Screen" 
-      options={{ title: 'Apps'}} />
+      options={
+            {   title: 'Apps', 
+                tabBarIcon: ({focused})=>
+                (
+                <Octicons name="apps" 
+                    size={24} 
+                    color={focused ? theme.lava : theme.dustyGrey} 
+                    />
+                )
+            }
+        } />
       <Tabs.Screen 
       name="clockin_Screen" 
-      options={{ title: 'Clock'}} />
+      options={
+        {   title: 'Clock',
+            tabBarIcon: ({focused})=>
+            (
+            <FontAwesome6 name="clock"
+             size={24} 
+             color={focused ? theme.lava : theme.dustyGrey}
+             />
+            )
+        }
+      } />
       <Tabs.Screen 
       name="timecard_Screen" 
-      options={{ title: 'Timecard'}} />
+      options={
+        {   title: 'Timecard',
+            tabBarIcon: ({focused})=>
+            (<MaterialCommunityIcons name="timetable" 
+                size={24} 
+                color={focused ? theme.lava : theme.dustyGrey} />)
+        }
+      } />
       <Tabs.Screen 
       name="profile_Screen" 
-      options={{ title: 'Profile'}} />
+      options={
+        { title: 'Profile',
+            tabBarIcon: ({focused})=>
+            (<Ionicons name="person" 
+                size={24} 
+                color={focused ? theme.lava : theme.dustyGrey} />)
+        }
+        } />
     </Tabs>
   )
 }
